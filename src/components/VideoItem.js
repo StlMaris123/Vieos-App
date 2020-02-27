572a0316c6bce85c239e357e7d2df1e74fb3fcda
StlMaris123 <stellamarisnjage@gmail.com>
2020-02-27 22:58:49 +0200

    styling a list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './VideoItem.css'
 
 const VideoItem = ({videos}) => {
     return(
        
-        <div>
-             <img src={videos.snippet.thumbnails.medium.urls} />
-            {videos.snippet.tittle}
+        <div className="video-item item">
+             <img className="ui image" src={videos.snippet.thumbnails.medium.urls} />
+            <div className="content">
+                <div className="header">{videos.snippet.tittle}</div>
+            </div>
         </div>
     )
 
